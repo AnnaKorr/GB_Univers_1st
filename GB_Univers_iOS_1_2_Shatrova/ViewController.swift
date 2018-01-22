0365297b6beb18f59cfdf7456a73e56fbbdadda5
AnnaKorr <7319162@gmail.com>
2018-01-23 00:46:57 +0300

    Initial Commit

---
+++ GB_Univers_iOS_1_2_Shatrova/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GB_Univers_iOS_1_2_Shatrova
+//
+//  Created by apple on 23.01.2018.
+//  Copyright © 2018 Korona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
